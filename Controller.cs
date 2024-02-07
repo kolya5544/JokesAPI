7868d778e6cdb4be6497833542e00b0a576e6063
kolya5544 <kolya@iktm.me>
2024-02-07 23:54:39 +0300

    initial commit

---
+++ Controller.cs
@@ -0,0 +1,60 @@
+﻿using EmbedIO.Routing;
+using EmbedIO;
+using EmbedIO.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokesAPI
+{
+    internal class Controller : WebApiController
+    {
+        public static string GetJoke()
+        {
+            return Program.jokes[Program.rng.Next(0, Program.jokes.Count)];
+        }
+
+        [Route(HttpVerbs.Get, "/")]
+        public async Task<string> GetRandomJoke()
+        {
+            return GetJoke();
+        }
+
+        [Route(HttpVerbs.Get, "/{friend}")]
+        public async Task<string> GetFriendJoke(string friend)
+        {
+            return $"{friend} said: {GetJoke()}";
+        }
+
+        [Route(HttpVerbs.Get, "/multi/{friend}")]
+        public async Task<string> GetMultipleJokes(string friend, [QueryField] int num)
+        {
+            string result = "";
+
+            for (int i = 0; i < num; i++)
+            {
+                result += $"{friend} tells his joke #{i + 1}: {GetJoke()}";
+            }
+
+            return result;
+        }
+
+        [Route(HttpVerbs.Post, "/")]
+        public async Task<Joke> GetJokePost([JsonData] JokeRequest jr)
+        {
+            return new Joke() { friend = jr.friend, joke = GetJoke() };
+        }
+    }
+
+    public class JokeRequest
+    {
+        public string friend { get; set; }
+    }
+    
+    public class Joke : JokeRequest
+    {
+        public string joke { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using EmbedIO;
+using EmbedIO.WebApi;
+using System;
+
+namespace JokesAPI
+{
+    internal class Program
+    {
+        public static Random rng = new Random();
+        public static List<string> jokes = new List<string>() {
+            "Why can't a bicycle stand on its own? It's two-tired.",
+            "I'm thinking of reasons to go to Switzerland. The flag is a big plus.",
+            "How do you find Will Smith in the snow? Look for fresh prints.",
+            "A plateau is the highest form of flattery.",
+            "Found out I was color blind the other day. That one came right out of the orange.",
+            "Where do generals keep their armies? In their sleevies!"
+        };
+
+        static void Main(string[] args)
+        {
+            var w = CreateWebServer("http://127.0.0.1:8088");
+            while (true) Thread.Sleep(1000);
+        }
+
+        private static WebServer CreateWebServer(string url)
+        {
+            var server = new WebServer(o => o
+                    .WithUrlPrefix(url)
+                    .WithMode(HttpListenerMode.EmbedIO))
+                .WithWebApi("/", m => m
+                    .WithController<Controller>());
+
+            server.Start();
+
+            return server;
+        }
+    }
+}
